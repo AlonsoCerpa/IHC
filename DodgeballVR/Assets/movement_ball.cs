b174ed367f0bba4a053c353ee7458d4d6c9a204b
Alonso Cerpa <alonso.cerpa@ucsp.edu.pe>
2018-09-06 06:43:46 -0500

    t

---
+++ DodgeballVR/Assets/movement_ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_ball : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float topForce = 100f;
+    public float sidewaysForce = 100f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Add a forward force
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, topForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
